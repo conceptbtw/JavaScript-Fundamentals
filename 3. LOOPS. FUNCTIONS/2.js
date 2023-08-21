a67467cec87b0c1cc5f1dce84f2081c792cc39c1
Illia Movchko <96948020+conceptbtw@users.noreply.github.com>
2023-08-21 08:39:50 +0000

    2 task

---
+++ 3. LOOPS. FUNCTIONS/2.js
@@ -0,0 +1,8 @@
+function printWithRepeat(a, b) {
+    for (let i = a; i <= b; i++) {
+      for (let j = 0; j <= i - a; j++) {
+        console.log(i);
+      }
+    }
+  }
+  printWithRepeat(1, 5);
