9d5cd7c4a46690e97b8a14ddeee2a1f471bf747e
Illia Movchko <96948020+conceptbtw@users.noreply.github.com>
2023-08-22 15:04:28 +0000

    7 task

---
+++ 3. LOOPS. FUNCTIONS/7.js
@@ -0,0 +1,13 @@
+function findUnique(arr) {
+    const uniqArr = [];
+    for (const num of arr) {
+      if (uniqArr.includes(num)) {
+        return false;
+      }
+      uniqArr.push(num);
+    }
+    return true;
+}
+console.log(findUnique([1, 2, 3, 5, 3]));
+console.log(findUnique([1, 2, 3, 5, 11]));
+  
