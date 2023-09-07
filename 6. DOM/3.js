8aa7e9b868fccc79d10bc540ddea7387451a5c7b
Illia Movchko <96948020+conceptbtw@users.noreply.github.com>
2023-09-07 06:46:49 +0000

    3 DOM

---
+++ 6. DOM/3.js
@@ -0,0 +1,11 @@
+const body = document.createElement("body");
+const main = document.createElement("main");
+const div = document.createElement("div");
+const p = document.createElement("p");
+main.className = "mainClass check item";
+div.id = "myDiv";
+p.textContent = "First paragraph";
+div.appendChild(p);
+main.appendChild(div);
+body.appendChild(main);
+document.documentElement.appendChild(body);
